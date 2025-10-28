e143b3969f83f9cbb7b7aa8baf2831b3f3460e1d
Rehan <ansarifaruq222@gmail.com>
2025-10-28 20:06:32 +0530

    Refactor number guessing game code
    Refactor number guessing game for clarity and maintainability.

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-import random
-n = random.randint(1,100)
-a = -1
-guesses = 1
-while(a !=n):
-    guesses +=1
-    a = int(input("Guess the number: "))
-    if( a>n):
+import random     #import random 
+n = random.randint(1,100)  #generate random number between 1 to 100 using random.randint
+a = -1  
+guesses = 1            #start the guess counter at 1
+while(a !=n):          #keep looping until condition is true
+    guesses +=1         
+    a = int(input("Guess the number: "))   
+    if( a>n):              
         print("Lower number please")
     elif(a<n):
-        print("Higher number please")
-        guesses +=1
+        print("Higher number please")       
 
 print(f"You have guessed the number {n} in {guesses} attempt")
+
